b3373f6a173ec532c90002616ffa80f4d2b3c3c2
hariprakashpanchareddi <75087920+hariprakashpanchareddi@users.noreply.github.com>
2022-07-09 14:34:46 +0530

    Add files via upload

---
+++ Drowsiness Detection Project/Drowsiness_Detection.py
@@ -0,0 +1,54 @@
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import dlib
+import cv2
+
+def eye_aspect_ratio(eye):
+	A = distance.euclidean(eye[1], eye[5])
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+	
+thresh = 0.25
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+cap=cv2.VideoCapture(0)
+flag=0
+while True:
+	ret, frame=cap.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	subjects = detect(gray, 0)
+	for sub in subjects:
+		shape = predict(gray, sub)
+		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+		l_eye = shape[lStart:lEnd]
+		r_eye = shape[rStart:rEnd]
+		l_ear = eye_aspect_ratio(l_eye)
+		r_ear = eye_aspect_ratio(r_eye)
+		ear = (l_ear + r_ear) / 2.0
+		l_eyeHull = cv2.convexHull(l_eye)
+		r_eyeHull = cv2.convexHull(r_eye)
+		if ear < thresh:
+			flag += 1
+			print (flag)
+			if flag >= frame_check:
+				cv2.putText(frame, "****************ALERT!****************", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				cv2.putText(frame, "****************ALERT!****************", (10,325),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				#print ("Drowsy")
+		else:
+			flag = 0
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+cv2.destroyAllWindows()
+cap.release() 
